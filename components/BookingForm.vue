9941d5f165c92a8a1e2ee75a0e47bdf233b81343
Aykol Koykelov <kolyakoikelov@gmail.com>
2019-07-01 17:30:04 +0600

    + added datepicker

--- components/BookingForm.vue
+++ components/BookingForm.vue
@@ -3,20 +3,29 @@
         .flexGrid
             .flexItem
                 div
-                    input(placeholder="Выберите дату заезда" type="text" v-model="coming_date")
+                    Datetime(placeholder="Выберите дату заезда" type="text" v-model="coming_date" required="'required'",
+                            format="yyyy-MM-dd" :phrases="dt_phrases")
+                    span(v-for="e in errors.collect('coming_date')") {{ e }}
                 div
-                    input(placeholder="Ваше имя" type="text" v-model="full_name")
+                    input(placeholder="Ваше имя" type="text" v-model="full_name" required="'required'")
+                    span(v-for="e in errors.collect('full_name')") {{ e }}
                 div
-                    input(placeholder="Номер телефона" type="text" v-model="phone")
+                    input(placeholder="Номер телефона" type="text" v-model="phone" required="'required'")
+                    span(v-for="e in errors.collect('phone')") {{ e }}
                 div
-                    input(placeholder="Гостей" type="number" v-model="adult_quantity")
+                    input(placeholder="Гостей" type="number" v-model="adult_quantity" required="'required'")
+                    span(v-for="e in errors.collect('adult_quantity')") {{ e }}
                 div
-                    input(placeholder="Детей" type="number" v-model="children_quantity")
+                    input(placeholder="Детей" type="number" v-model="children_quantity" required="'required'")
+                    span(v-for="e in errors.collect('children_quantity')") {{ e }}
             .flexItem
                 div
-                    input(placeholder="Выберите дату выезда" type="text" v-model="leaving_date")
+                    Datetime(placeholder="Выберите дату выезда" type="text" v-model="leaving_date" required="'required'",
+                             format="yyyy-MM-dd" :phrases="dt_phrases")
+                    span(v-for="e in errors.collect('leaving_date')") {{ e }}
                 div
-                    input(placeholder="Электронная почта" type="text" v-model="email")
+                    input(placeholder="Электронная почта" type="email" v-model="email" required="'required'")
+                    span(v-for="e in errors.collect('email')") {{ e }}
                 div
                     .nameForm Выберите номер (можно несколько)
                     .flexGridInner
@@ -30,8 +39,14 @@
 </template>
 
 <script>
+  import { Datetime } from 'vue-datetime';
+  import 'vue-datetime/dist/vue-datetime.css'
+
   export default {
     name: 'BookingForm',
+    components: {
+        Datetime
+    },
     props: {
         rooms_choices: {
             type: Array,
@@ -50,20 +65,32 @@
             leaving_date: '',
             email: '',
             rooms: [],
+            dt_phrases: {
+                ok: 'OK',
+                cancel: 'Отмена'
+            }
         }
     },
     methods: {
         async createBooking() {
-            // let response = await this.$api('post', '/booking/create/', {
-            //     leaving_date: this.leaving_date,
-            //     adult_quantity: this.adult_quantity,
-            //     coming_date: this.coming_date,
-            //     children_quantity: this.children_quantity,
-            //     phone: this.phone,
-            //     rooms: this.rooms,
-            //     full_name: this.full_name,
-            //     email: this.email,
-            // })
+            this.$validate(this, async () => {
+                let result = await this.$api('post', '/booking/create/', {
+                    leaving_date: this.leaving_date,
+                    adult_quantity: this.adult_quantity,
+                    coming_date: this.coming_date,
+                    children_quantity: this.children_quantity,
+                    phone: this.phone,
+                    rooms: this.rooms,
+                    full_name: this.full_name,
+                    email: this.email,
+                })
+
+                if (result['success']) {
+
+                } else {
+                    this.$pushErrors(this, result['response'].data)
+                }
+            })
         },
         getImage(gallery) {
             return gallery.length > 0 ? gallery[0].image : null

b75c592484d59c2cf7d6e3add927b590c582b847
Aykol Koykelov <kolyakoikelov@gmail.com>
2019-07-05 16:48:20 +0600

    ^ set map coords

--- components/footers/ThirdFooter.vue
+++ components/footers/ThirdFooter.vue
@@ -20,7 +20,7 @@
                         .margin
 
                 .right
-                    iframe(src='https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d11695.94529647709!2d74.6283202!3d42.8731328!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2skg!4v1560850063377!5m2!1sru!2skg', width='100%', height='100%', frameborder='0', style='border:0', allowfullscreen='')
+                    iframe(src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2925.6965390791383!2d74.5980483!3d42.8370236!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x389eb73d3d024303%3A0xf6571c1c345101ac!2s%22Oasis+Residence%22!5e0!3m2!1sru!2skg!4v1562323524453!5m2!1sru!2skg', width='100%', height='100%', frameborder='0', style='border:0', allowfullscreen='')
         .sunrise
             a(href="http://sunrisestudio.pro/") Сделано в Sunrise
 </template>
